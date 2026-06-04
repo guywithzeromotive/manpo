2bc92405e0cddc5930f3bf94659492b2abe27d1b
guywithzeromotive <ntnesi01@gmail.com>
2026-06-04 17:19:15 +0300

    feat: default constructor and (getter, setter)

--- src/main/java/com/zero/manpo/models/Report.java
+++ src/main/java/com/zero/manpo/models/Report.java
@@ -7,4 +7,43 @@ public class Report {
     private String projectLink;
     private String projectDescription;
     private String projectDeepDive;
+
+    Report(String projectName, String projectLink, String projectDescription, String projectDeepDive){
+        this.projectName = projectName;
+        this.projectLink = projectLink;
+        this.projectDescription =  projectDescription;
+        this.projectDeepDive = projectDeepDive;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void setProjectLink(String projectLink) {
+        this.projectLink = projectLink;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public void setProjectDeepDive(String projectDeepDive) {
+        this.projectDeepDive = projectDeepDive;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getProjectLink() {
+        return projectLink;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public String getProjectDeepDive() {
+        return projectDeepDive;
+    }
 }
